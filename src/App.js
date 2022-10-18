3a3d58ab605ddd0b81bf636bbd46fd2f9c760338
Frotan <frotan@MFROTANs-MacBook-Air.local>
2022-10-18 13:08:20 +0430

    v1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/headers/Header';
+import './App.css';
+import Footer from './components/Footer/Footer';
+function App() {
+  return (
+    <div>
+      <Header/>
+            
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
